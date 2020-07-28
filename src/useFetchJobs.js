6f4677c1b2bebc39c4935d0e4e0464e05011e0bf
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-28 10:00:19 +0100

    Setting up the fetch request to jobs site

---
+++ src/useFetchJobs.js
@@ -0,0 +1,45 @@
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+};
+
+const BASE_URL =
+  'https://jobs.github.com/positions.json?description=python&full_time=true&location=sf';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
+
+export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+
+    axios.get(BASE_URL, (params: { markdown: true }));
+  }, [params, page]);
+  return {
+    jobs: [],
+    loading: false,
+    error: false,
+  };
+}
