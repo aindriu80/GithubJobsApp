8998b4697cf177beb3d52d9ca29078b1c6d7118d
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-30 09:59:54 +0100

    Finishing the front end

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [page, setPage] = useState(1)
   const { jobs, loading, error } = useFetchJobs(params, page)
   return (
-    <Container>
+    <Container className="my-4">
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error Try refreshing.</h1>}
       {<h1>{jobs.length}</h1>}
