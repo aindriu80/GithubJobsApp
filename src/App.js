bcf96ee6684432aed0a516c89e34d7af2233af33
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-08-01 15:55:33 +0100

    Completing the Jobs App

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import { Container } from 'react-bootstrap'
 import Job from './Job'
 import './App.css'
 import JobsPagination from './JobsPagination'
+import SearchForm from './SearchForm'
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value }
+    })
+  }
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs App</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h3>Loading...</h3>}
       {error && <h3>Error Try refreshing.</h3>}
